def8ad0a38a1aa76d8aa74810a0ea9ee749043c0
Alexander Semenyuk <facemmannn403@gmail.com>
2021-12-05 09:08:33 +0300

    initializing the project from spring initializr

---
+++ src/main/java/com/github/justalexandeer/SocialNewsAppBackend/SocialNewsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.justalexandeer.SocialNewsAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNewsAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNewsAppBackendApplication.class, args);
+	}
+
+}
